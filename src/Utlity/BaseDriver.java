bfbf828a07b1db853904549978515493dbcdce6b
ZeynepBaglars <zeynepbaglars@gmail.com>
2023-08-31 06:30:25 +0300

    hepsi bitti

--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -19,7 +19,7 @@ public class BaseDriver {
         logger.setLevel(Level.SEVERE); // sadece ERROR ları göster
 
         driver = new ChromeDriver();
-        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
